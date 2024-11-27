2d2a7583073e964db38fd7f46784696ad7d227c3
rahmsc <danmilad.code@gmail.com>
2024-11-27 16:12:35 +0700

    Updated for vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "product.rahms.dev",
+          },
+        ],
+        destination: "https://rahms.dev/product",
+        permanent: true,
+      },
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "blog.rahms.dev",
+          },
+        ],
+        destination: "https://rahms.dev/blog",
+        permanent: true,
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+import { Anton } from "next/font/google";
+
+const anton = Anton({
+  subsets: ["latin"],
+  variable: "--font-anton",
+  weight: "400",
 });
 
 export const metadata: Metadata = {
@@ -25,11 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${anton.variable} antialiased`}>{children}</body>
     </html>
   );
 }
